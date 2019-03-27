820a9173184f7db440e59cec4c9c2a3e07e8e31f
gokselaktas <aktas_goksel@hotmail.com>
2019-03-27 23:23:05 +0300

    kisakenar uzunkenar yamuk vs...

---
+++ integral alma yaklasimlari.py
@@ -0,0 +1,31 @@
+def f(x):
+    return x**2
+
+
+x1 , x2 = 4, 6
+n = 1000000  # araligi kac parcaya bolecegiz
+h = (x2-x1)/n  # her bir dikdortgenin taban uzunlugu
+
+#kisakenar integral
+integral = 0
+for i in range(n):   # alt dikdortgen toplam
+   integral += f(x1 + i*h)*h
+print(integral)
+
+#uzunkenar integral
+integral = 0
+for i in range (0,n+1):   # ust dikdortgen toplam
+    integral += f(x1+i*h)*h
+print(integral)
+
+#Bu iki metodun ortalamasi kisa-uzun
+integral = 0
+for i in range (n):
+    integral += f(x1 + h/2 + i*h)*h
+print(integral)
+
+#Yamugun alani yardimi ile integral
+integral = 0
+for i in range (n):
+    integral += (f(x1+i*h) + f(x1+(i+1)*h))*h/2
+print(integral)
